88b63cb64f0bd474ab6d43c2caf5b312ebdc5f14
Dave Rolsky <autarch@urth.org>
2013-08-29 15:51:44 -0500

    This works for IPv4 addresses but not IPv6
    I only needed IPv4 for now but I will try to get back to this in the future.

--- lib/MaxMind/DB/Reader/Role/Reader.pm
+++ lib/MaxMind/DB/Reader/Role/Reader.pm
@@ -7,6 +7,7 @@ use autodie;
 
 use Data::Validate::IP 0.16
     qw( is_ipv4 is_ipv6 is_private_ipv4 is_private_ipv6 );
+use Math::Int128 qw( uint128 );
 use Net::Works::Address 0.12;
 
 use Moo::Role;
@@ -30,6 +31,53 @@ sub record_for_address {
         if is_private_ipv4($addr) || is_private_ipv6($addr);
 
     return $self->_data_for_address($addr);
+}
+
+sub iterate_search_tree {
+    my $self     = shift;
+    my $callback = shift;
+
+    my $node_num  = 0;
+    my $ipnum     = $self->ip_version() == 4 ? 0 : uint128(0);
+    my $depth     = 1;
+    my $max_depth = $self->ip_version() == 4 ? 32 : 128;
+
+    $self->_iterate_search_tree( $callback, $node_num, $ipnum, $depth, $max_depth );
+}
+
+sub _iterate_search_tree {
+    my $self     = shift;
+    my $callback = shift;
+    my $node_num = shift;
+    my $ipnum    = shift;
+    my $depth    = shift;
+    my $max_depth = shift;
+
+    no warnings 'recursion';
+
+    my ( $left, $right ) = $self->_read_node($node_num);
+    for my $value ( $left, $right ) {
+
+        # We ignore empty branches of the search tree
+        next if $value == $self->node_count();
+
+        my $one = $self->ip_version() == 4 ? 1 : uint128(1);
+
+        $ipnum |= ( $one << ( $max_depth - $depth ) ) if $value == $right;
+
+        if ( $value <= $self->node_count() ) {
+            $self->_iterate_search_tree(
+                $callback, $value, $ipnum,
+                $depth + 1, $max_depth,
+            );
+        }
+        else {
+            $callback->(
+                $ipnum, $depth,
+                $self->_resolve_data_pointer($value)
+            );
+        }
+    }
 }
 
 around _build_metadata => sub {
