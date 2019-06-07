9f03457fdb27e09e4452c15e41a81167bbb7cc2b
Mark Fowler <mark@twoshortplanks.com>
2019-06-07 18:06:31 +0000

    add deprecation notice

--- lib/MaxMind/DB/Reader.pm
+++ lib/MaxMind/DB/Reader.pm
@@ -139,5 +139,8 @@ major number, YYY is the minor number, and ZZZ is the patch number.
 
 =head1 SUPPORT
 
+This module is deprecated and will only receive fixes for major bugs and
+security vulnerabilities. New features and functionality will not be added.
+
 Please report all issues with this code using the GitHub issue tracker at
 L<https://github.com/maxmind/MaxMind-DB-Reader-perl/issues>.
